8343401ad988d1f4034a067c0bccf17067246ca9
rpfeifer <rene_pfeifer@kabelbw.de>
2020-11-01 17:03:18 +0100

    Improve linting and prettier. Improve localization.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,156 +12,141 @@ https://github.com/typescript-eslint/tslint-to-eslint-config/blob/master/docs/FA
 Happy linting! 💖
 */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "google",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "./src/tsconfig.test.json",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "require-jsdoc": "off",
-        "@typescript-eslint/dot-notation": "error",
-        "@typescript-eslint/indent": [
-            "off",
-            4,
-            {
-                "CallExpression": {
-                    "arguments": "first"
-                },
-                "FunctionDeclaration": {
-                    "parameters": "first"
-                },
-                "FunctionExpression": {
-                    "parameters": "first"
-                }
-            }
-        ],
-        "@typescript-eslint/member-delimiter-style": [
-            "error",
-            {
-                "multiline": {
-                    "delimiter": "semi",
-                    "requireLast": true
-                },
-                "singleline": {
-                    "delimiter": "semi",
-                    "requireLast": false
-                }
-            }
-        ],
-        "@typescript-eslint/member-ordering": "error",
-        "@typescript-eslint/no-empty-function": "error",
-        "@typescript-eslint/no-explicit-any": "error",
-        "@typescript-eslint/no-misused-new": "error",
-        "@typescript-eslint/no-unused-expressions": "error",
-        "@typescript-eslint/no-use-before-define": "error",
-        "@typescript-eslint/quotes": [
-            "error",
-            "single"
-        ],
-        "@typescript-eslint/semi": [
-            "error",
-            "always"
-        ],
-        "@typescript-eslint/type-annotation-spacing": "error",
-        "brace-style": [
-            "error",
-            "1tbs"
-        ],
-        "camelcase": "error",
-        "comma-dangle": "off",
-        "curly": "error",
-        "default-case": "error",
-        "eol-last": "off",
-        "eqeqeq": [
-            "error",
-            "always"
-        ],
-        "guard-for-in": "error",
-        "id-blacklist": [
-            "error",
-            "any",
-            "Number",
-            "number",
-            "String",
-            "string",
-            "Boolean",
-            "boolean",
-            "Undefined",
-            "undefined"
-        ],
-        "id-match": "error",
-        "max-len": [
-            "error",
-            {
-                "code": 120
-            }
-        ],
-        "no-bitwise": "error",
-        "no-caller": "error",
-        "no-console": [
-            "error",
-            {
-                "allow": [
-                    "warn",
-                    "dir",
-                    "timeLog",
-                    "assert",
-                    "clear",
-                    "count",
-                    "countReset",
-                    "group",
-                    "groupEnd",
-                    "table",
-                    "dirxml",
-                    "groupCollapsed",
-                    "Console",
-                    "profile",
-                    "profileEnd",
-                    "timeStamp",
-                    "context"
-                ]
-            }
-        ],
-        "no-debugger": "error",
-        "no-empty": "error",
-        "no-eval": "error",
-        "no-fallthrough": "error",
-        "no-multiple-empty-lines": "error",
-        "no-new-wrappers": "error",
-        "no-redeclare": "error",
-        "no-shadow": [
-            "error",
-            {
-                "hoist": "all"
-            }
-        ],
-        "no-trailing-spaces": "off",
-        "no-underscore-dangle": "error",
-        "no-unused-labels": "error",
-        "no-var": "error",
-        "radix": "error",
-        "spaced-comment": [
-            "error",
-            "always",
-            {
-                "markers": [
-                    "/"
-                ]
-            }
-        ]
-    }
+   env: {
+      browser: true,
+      es6: true,
+      node: true,
+   },
+   extends: [
+      "google",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier",
+   ],
+   parser: "@typescript-eslint/parser",
+   parserOptions: {
+      project: "./src/tsconfig.test.json",
+      sourceType: "module",
+   },
+   plugins: ["@typescript-eslint"],
+   ignorePatterns: ["i18n.js", "*.cjs"],
+   rules: {
+      "require-jsdoc": "off",
+      "@typescript-eslint/dot-notation": "error",
+      "@typescript-eslint/indent": [
+         "off",
+         4,
+         {
+            CallExpression: {
+               arguments: "first",
+            },
+            FunctionDeclaration: {
+               parameters: "first",
+            },
+            FunctionExpression: {
+               parameters: "first",
+            },
+         },
+      ],
+      "@typescript-eslint/member-delimiter-style": [
+         "error",
+         {
+            multiline: {
+               delimiter: "semi",
+               requireLast: true,
+            },
+            singleline: {
+               delimiter: "semi",
+               requireLast: false,
+            },
+         },
+      ],
+      "@typescript-eslint/member-ordering": "error",
+      "@typescript-eslint/no-empty-function": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-misused-new": "error",
+      "@typescript-eslint/no-unused-expressions": "error",
+      "@typescript-eslint/no-use-before-define": "error",
+      "@typescript-eslint/quotes": ["error", "single"],
+      "@typescript-eslint/semi": ["error", "always"],
+      "@typescript-eslint/type-annotation-spacing": "error",
+      "brace-style": ["error", "1tbs"],
+      camelcase: "error",
+      "comma-dangle": "off",
+      curly: "error",
+      "default-case": "error",
+      "eol-last": "off",
+      eqeqeq: ["error", "always"],
+      "guard-for-in": "error",
+      "id-blacklist": [
+         "error",
+         "any",
+         "Number",
+         "number",
+         "String",
+         "string",
+         "Boolean",
+         "boolean",
+         "Undefined",
+         "undefined",
+      ],
+      "id-match": "error",
+      "max-len": [
+         "error",
+         {
+            code: 120,
+         },
+      ],
+      "no-bitwise": "error",
+      "no-caller": "error",
+      "no-console": [
+         "error",
+         {
+            allow: [
+               "warn",
+               "dir",
+               "timeLog",
+               "assert",
+               "clear",
+               "count",
+               "countReset",
+               "group",
+               "groupEnd",
+               "table",
+               "dirxml",
+               "groupCollapsed",
+               "Console",
+               "profile",
+               "profileEnd",
+               "timeStamp",
+               "context",
+            ],
+         },
+      ],
+      "no-debugger": "error",
+      "no-empty": "error",
+      "no-eval": "error",
+      "no-fallthrough": "error",
+      "no-multiple-empty-lines": "error",
+      "no-new-wrappers": "error",
+      "no-redeclare": "error",
+      "no-shadow": [
+         "error",
+         {
+            hoist: "all",
+         },
+      ],
+      "no-trailing-spaces": "off",
+      "no-underscore-dangle": "error",
+      "no-unused-labels": "error",
+      "no-var": "error",
+      radix: "error",
+      "spaced-comment": [
+         "error",
+         "always",
+         {
+            markers: ["/"],
+         },
+      ],
+   },
 };
--- i18n.js
+++ i18n.js
@@ -1,74 +1,133 @@
 import InitCmd from "ttag-cli/dist/src/commands/init.js";
+import CheckCmd from "ttag-cli/dist/src/commands/check.js";
 import UpdateCmd from "ttag-cli/dist/src/commands/update.js";
+import WebCmd from "ttag-cli/dist/src/commands/web.js";
 import parser_1 from "ttag-cli/dist/src//lib/parser.js";
 import utils_1 from "ttag-cli/dist/src//lib/utils.js";
-import fs from 'fs';
-import glob from 'glob';
+import fs from "fs";
+import glob from "glob";
+import yargs from "yargs";
 
-var __values = (this && this.__values) || function(o) {
-   var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
-   if (m) return m.call(o);
-   if (o && typeof o.length === "number") return {
-       next: function () {
-           if (o && i >= o.length) o = void 0;
-           return { value: o && o[i++], done: !o };
-       }
+var __values =
+   (this && this.__values) ||
+   function (o) {
+      var s = typeof Symbol === "function" && Symbol.iterator,
+         m = s && o[s],
+         i = 0;
+      if (m) return m.call(o);
+      if (o && typeof o.length === "number")
+         return {
+            next: function () {
+               if (o && i >= o.length) o = void 0;
+               return { value: o && o[i++], done: !o };
+            },
+         };
+      throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
    };
-   throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
-};
 
 function po2json(lang, path, pretty, nostrip, format) {
    var e_1, _a;
-   if(!fs.existsSync(path)) {
-        const init = InitCmd.default;
-        init(lang, path);
+   if (!fs.existsSync(path)) {
+      const init = InitCmd.default;
+      init(lang, path);
    }
    var poData = parser_1.parse(fs.readFileSync(path).toString());
    var messages = utils_1.iterateTranslations(poData.translations);
    if (!nostrip) {
-       var header = messages.next().value;
-       delete header.comments;
-       try {
-           for (var messages_1 = __values(messages), messages_1_1 = messages_1.next(); !messages_1_1.done; messages_1_1 = messages_1.next()) {
-               var msg = messages_1_1.value;
-               delete msg.comments;
-           }
-       }
-       catch (e_1_1) { e_1 = { error: e_1_1 }; }
-       finally {
-           try {
-               if (messages_1_1 && !messages_1_1.done && (_a = messages_1.return)) _a.call(messages_1);
-           }
-           finally { if (e_1) throw e_1.error; }
-       }
+      var header = messages.next().value;
+      delete header.comments;
+      try {
+         for (
+            var messages_1 = __values(messages), messages_1_1 = messages_1.next();
+            !messages_1_1.done;
+            messages_1_1 = messages_1.next()
+         ) {
+            var msg = messages_1_1.value;
+            delete msg.comments;
+         }
+      } catch (e_1_1) {
+         e_1 = { error: e_1_1 };
+      } finally {
+         try {
+            if (messages_1_1 && !messages_1_1.done && (_a = messages_1.return)) _a.call(messages_1);
+         } finally {
+            if (e_1) throw e_1.error;
+         }
+      }
    }
    if (format === "compact") {
-       poData = utils_1.convert2Compact(poData);
+      poData = utils_1.convert2Compact(poData);
    }
    return JSON.stringify(poData, null, pretty ? 2 : 0);
 }
 
-function scan(lang) {
-    const po = 'src/shared/i18n/'+lang+'.po';
-    const pj = 'src/shared/i18n/'+lang+'.ts';
-    const update = UpdateCmd.default;
-    glob('./src/**/!(i18n)/*.ts?(x)', function(er, files) {
-       update(po, files, lang, {
-          discover: ['t', 'ngettext'],
-          numberedExpressions: true,
-          sortByMsgid: true,
-          addComments: 'translate:'
-       })
-    
-       fs.writeFile(pj, `export const locale: any = 
-       ${po2json(lang, po, false, false, 'compact')};`, function(err) {
-          if(err) {
-             throw err;
-          }
-       });
-       console.log('Written '+pj);
-    });
+let options = {
+   discover: ["t", "ngettext"],
+   numberedExpressions: true,
+   sortByMsgid: true,
+   addComments: "translate:",
+};
+
+function poFile(lang) {
+   return "src/shared/i18n/" + lang + ".po";
 }
 
-// List supported languages
-scan('en');
+function tsFile(lang) {
+   return "src/shared/i18n/" + lang + ".ts";
+}
+
+function handleFiles(fct) {
+   glob("./src/**/!(i18n)/*.ts?(x)", function (er, files) {
+      fct(files);
+   });
+}
+
+function check(lang) {
+   handleFiles((files) => {
+      CheckCmd.default(poFile(lang), files, lang, options);
+   });
+}
+
+function update(lang) {
+   handleFiles((files) => {
+      UpdateCmd.default(poFile(lang), files, lang, options);
+
+      fs.writeFile(
+         tsFile(lang),
+         `export const locale: any = 
+          ${po2json(lang, poFile(lang), false, false, "compact")};`,
+         function (err) {
+            if (err) {
+               throw err;
+            }
+         }
+      );
+   });
+}
+
+function translate(lang) {
+   WebCmd.default(poFile(lang));
+}
+
+// Analyze
+let args = yargs(process.argv.slice(2))
+   .command("check", "Check whether the translations are complete!")
+   .command("update", "Update the translations!")
+   .command("translate", "Translate the text by opening the web-app!")
+   .demandCommand(1)
+   .help()
+   .alias("help", "h").argv;
+
+switch (args._[0]) {
+   case "check":
+      check("en");
+      break;
+
+   case "update":
+      update("en");
+      break;
+
+   case "translate":
+      translate("en");
+      break;
+}
